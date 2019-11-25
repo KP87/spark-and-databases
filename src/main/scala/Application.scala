2320f5546fc50e1c398c1be3fa42305de0fd6a13
KP87 <karpisarek@gmail.com>
2019-11-25 10:57:13 +0100

    Improved time of getting list of files in directory

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,12 +1,8 @@
 import java.io.File
-import java.nio.file.{Files, Paths}
 import java.sql.Timestamp
 import java.util.Properties
-
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
-
-import scala.collection.JavaConverters._
 import scala.io.Source
 
 object Application extends App {
@@ -27,25 +23,23 @@ object Application extends App {
   val USER_HOME_DIR = System.getProperty("user.home")
   val sourceDataPath = s"$USER_HOME_DIR/Downloads/release/taxi_log_2008_by_id"
 
-  val listOfFiles: List[String] = Files.walk(Paths.get(sourceDataPath))
-    .iterator()
-    .asScala
-    .filter(Files.isRegularFile(_))
-    .map(_.toString)
+  val listOfFiles: List[String] = new File(sourceDataPath)
+    .listFiles()
+    .filter(_.isFile)
+    .map(_.getAbsolutePath)
     .toList
 
   import spark.implicits._
-  // Approach 1 (write content of each file to database sequentially)
-  writeSequentially(listOfFiles)
-  // Approach 2 (concat content of each file parallel and write all data to database in one operation)
+  // Approach 1 : write content of each file to database sequentially
+//  writeSequentially(listOfFiles)
+  // Approach 2 : concat content of each file parallel and write all data to database in one operation
   writeParallel(listOfFiles)
 
-  // Loading data from a JDBC source
+  // Reading data from a JDBC source
   val jdbcDF: DataFrame = spark.read.jdbc(url, tableName, connectionProperties)
   println(jdbcDF.show)
 
   def writeSequentially(listOfFiles: List[String]) = {
-    val startTime: Long = System.nanoTime()
     listOfFiles.foreach(fileDir => {
       val positionsDF: DataFrame = spark.sparkContext
         .textFile(fileDir)
@@ -60,31 +54,25 @@ object Application extends App {
   }
 
   def writeParallel(listOfFiles: List[String]) = {
-    val startTime: Long = System.nanoTime()
-    
-    val taxiData: RDD[Position] = spark.sparkContext.parallelize(listOfFiles)
-      .repartition(2)
+    val taxiData: RDD[Position] = spark.sparkContext.parallelize(listOfFiles, 2)
       .mapPartitions(partition => {
         partition.map(fileDir => {
-          Source.fromFile(new File(fileDir))
+          val bufferedSource = Source.fromFile(fileDir)
+
+          val output = bufferedSource
             .getLines
-            .toList
+            .toVector
             .map(row => row.split(","))
             .map(row => Position(row(0).toInt, Timestamp.valueOf(row(1)), row(2).toDouble,  row(3).toDouble))
+
+          bufferedSource.close()
+          output
         })
       }).flatMap(x => x)
 
-    val elapsedTime: Long = (System.nanoTime() - startTime)
-    println(s"time of execution = $elapsedTime ns")
-
-    val startTime2: Long = System.nanoTime()
-    
     taxiData.toDF()
       .write
       .mode(SaveMode.Append)
       .jdbc(url, tableName, connectionProperties)
-
-    val elapsedTime2: Long = (System.nanoTime() - startTime2)
-    println(s"time of execution = $elapsedTime2 ns")
   }
 }
